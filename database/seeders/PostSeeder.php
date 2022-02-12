6b0cd6fabd89efe5d955586d769e5e49b45fb59c
DESKTOP-76J7K0E\Admin <Rezarostaminikoo@gmail.com>
2022-02-12 15:21:50 +0330

    add laravel actions

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use Database\Factories\PostFactory;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PostSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            PostSeeder::class,
-        ]);
+        PostFactory::new()->count(20)->create();
     }
 }
